a181a8221f2b1c9b02ff39232e68fafda5a24f74
fiuari <fiuari.federico@gmail.com>
2026-06-08 12:59:00 +0300

    feat: burger-menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
-console.log('Project started');
+const burger = document.querySelector(".burger");
+const header = document.querySelector(".header");
+
+burger.addEventListener("click", () => {
+  header.classList.toggle("open");
+  burger.classList.toggle("active");
+});
