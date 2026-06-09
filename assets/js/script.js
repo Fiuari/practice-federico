543ae6f6ebc44c2671f6de8c5109b1b4d5c83048
fiuari <fiuari.federico@gmail.com>
2026-06-09 17:26:00 +0300

    feat: swiper

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,27 @@ burger.addEventListener("click", () => {
   header.classList.toggle("open");
   burger.classList.toggle("active");
 });
+
+const wrappers = document.querySelectorAll(".rooms-wrapper");
+const arrows = document.querySelectorAll(".poly-arrow"); // кнопки зі стрілкою
+let currentIndex = 0;
+
+// функція показу потрібного слайду
+function showSlide(index) {
+  wrappers.forEach((wrapper, i) => {
+    wrapper.style.opacity = i === index ? "1" : "0";
+    wrapper.style.transform = i === index ? "translateX(0)" : "translateX(100%)";
+    wrapper.style.zIndex = i === index ? "1" : "0";
+  });
+}
+
+// початковий стан
+showSlide(currentIndex);
+
+// при кліку на стрілку
+arrows.forEach(arrow => {
+  arrow.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % wrappers.length; // зациклення
+    showSlide(currentIndex);
+  });
+});
