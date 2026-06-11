df5e3a7279445d91c91a9372b1a663fedf6970e9
fiuari <fiuari.federico@gmail.com>
2026-06-11 15:27:00 +0300

    feat: animation on scrool

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,3 +68,14 @@ accordionItems.forEach(item => {
 });
 
 
+const elements = document.querySelectorAll('.animate-on-scroll');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+
+elements.forEach(el => observer.observe(el));
